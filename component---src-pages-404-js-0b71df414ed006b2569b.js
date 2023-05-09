d1562410357c0a3611e82b0e6a74bca4b145e7ac
Dinar Davis <dinardavis@gmail.com>
2023-05-09 19:08:44 +0800

    Updates

---
+++ component---src-pages-404-js-0b71df414ed006b2569b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[883],{429:function(e,t,l){l.r(t),l.d(t,{default:function(){return a}});var n=l(7294),r=l(5730);function a(){return n.createElement(r.Z,null,n.createElement("main",{className:"error-page"},n.createElement("h1",null,"404"),n.createElement("h3",null,"page not ",n.createElement("br",null),n.createElement("span",null,"found"))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0b71df414ed006b2569b.js.map
