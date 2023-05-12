f38e4e8f8f5957a4a598c8003589a94d31cfb496
Dinar Davis <dinardavis@gmail.com>
2023-05-12 12:37:02 +0800

    Update site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,12 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  siteMetadata: {
+    title: `Donut Miss It!`,
+    description: `Donut Miss It is more than just a donut shop. Our stores are beloved parts of each of the communities we serve.`,
+    author: `@dinardavis`,
+    siteUrl: `https://dinardavis.github.io/donut_miss_out/`,
+  },
   pathPrefix: "/donut_miss_out",
   plugins: [`gatsby-plugin-smoothscroll`, 'gatsby-plugin-react-helmet',
     {
@@ -17,13 +23,6 @@ module.exports = {
       },
     },
   ],
-  siteMetadata: {
-    title: `Using Gatsby Head`,
-    description: `Example project for the Gatsby Head API`,
-    twitterUsername: `@gatsbyjs`,
-    image: `/gatsby-icon.png`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
 }
 
 
